ef72658ca7f4fe067c84039221d83558e6a7fb0c
Attila <attila.ocsag@gmail.com>
2024-04-19 22:42:01 +0200

    testing queries

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
   
   export const { sql, overwriteDatabaseFile, getDatabaseFile } = new SQLocal('ffd.db');
     
-  let id;
+  let searchText = "";
   let res = "";
   
   const fileInputChange = async()=> {
@@ -14,7 +14,8 @@
     } catch (error) {
         alert(error);
     } finally {
-      //id = "";
+      const version = await sql`select sqlite_version()`;
+      console.log(version[0]);
     }    
   }
 
@@ -32,46 +33,59 @@
 
   const query1 = async()=>{
     try {
-      const version = await sql`select sqlite_version()`;
-      console.log(version[0]);
-      id = `%${id}%`;
-      const data = await sql`select hun, ltn from taxons where lower(hun) LIKE ${id}`;
-      res = `${data[0].hun} (${data[0].ltn})`;
-      console.log(JSON.stringify(data));
+      searchText = `%${searchText}%`;
+      const data = await sql`select hun, ltn from taxons where lower(hun) LIKE ${searchText}`;
+      res = JSON.stringify(data);
     } catch (error) {
         console.log(error);
         alert(error);
     } finally {
-      id = "";
+      searchText = "";
     }
   }  
 
   const query2 = async()=>{
     try {
-      const version = await sql`select sqlite_version()`;
-      console.log(version[0]);
       const data = await sql`SELECT json_extract('{"a": 1, "b": 15}', '$.b') x;`;
       res = data[0].x;
     } catch (error) {
         console.log(error);
         alert(error);
     } finally {
-      id = "";
+      searchText = "";
+    }
+  }  
+
+  const query3 = async()=>{
+    try {
+      const data = await sql`select hun, ltn from taxons where lower(hun) REGEXP ${searchText}`;
+      res = data.length>0 ? `${data[0].hun} (${data[0].ltn})` : 'none';
+      console.log(JSON.stringify(data));
+    } catch (error) {
+        console.log(error);
+        alert(error);
+    } finally {
+      searchText = "";
     }
   }  
 
   
   </script>
   
-  <div class="flex flex-col gap-2 items-center bg-indigo-300">
-    <h1>Hello Attila</h1>
-    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+  <div class="flex flex-col gap-2 p-2 items-start bg-indigo-300 h-full">
     <input class="border-2 border-violet-400 rounded-sm" type="file" id="input" name="Input" accept=".db, .sqlite" on:change={fileInputChange}/>
-    <input class="border-2 border-violet-400 rounded-sm" type="text" on:change={fileInputChange} bind:value={id}/>
-    <div class="flex gap-2">
-      <button class="border-2 border-violet-400 rounded-md p-1" on:click={query1}>Query</button>
-      <button class="border-2 border-violet-400 rounded-md p-1" on:click={query2}>JSON Query</button>
-      <button class="border-2 border-violet-400 rounded-md p-1" on:click={extractFile}>Download</button>
+    <input 
+      class="border-2 border-violet-400 rounded-sm w-1/2" 
+      type="text" 
+      on:change={fileInputChange} 
+      bind:value={searchText}
+      placeholder="searchtext"
+    />
+    <div class="flex flex-col gap-2 p-2">
+      <button class="border-2 border-violet-400 rounded-md p-1" on:click={query1}>Normal query</button>
+      <button class="border-2 border-violet-400 rounded-md p-1" on:click={query3}>Regexp query</button>
+      <button class="border-2 border-violet-400 rounded-md p-1" on:click={query2}>JSON query</button>
+      <!--button class="border-2 border-violet-400 rounded-md p-1" on:click={extractFile}>Download</button-->
     </div>
-    <span class="font-bold">{res}</span>
+    <textarea class="p-2 w-full h-1/3" value={res} row=10/>
   </div>
